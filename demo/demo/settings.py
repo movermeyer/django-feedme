3ca07190fac06957133d1bdc6398fd325ec5a797
gotlium <death-metall@mail.ru>
2013-04-30 12:59:52 +0500

    static processors was added to demo settings

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -98,6 +98,7 @@ TEMPLATE_LOADERS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
+    'django.core.context_processors.static',
 )
 
 
