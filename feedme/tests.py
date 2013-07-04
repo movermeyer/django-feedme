a9575e9f1571f1507e74c1ae5675a1263219d4c0
Halldór Rúnarsson <halldor@unsupervised.home>
2013-07-04 12:47:23 +0000

    Demonstrate an error with the feed datastructure.

--- feedme/tests.py
+++ feedme/tests.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.test import TestCase
 
 from feedme.models import Feed, FeedItem
@@ -13,3 +15,25 @@ class FeedMeTestCase(TestCase):
 
     def test_me(self):
         self.assertTrue(True, True)
+
+    def test_multiple_users_same_feed(self):
+        user1 = User(username="user1")
+        user1.set_unusable_password()
+        user2 = User(username="user2")
+        user2.set_unusable_password()
+
+        user1.save()
+        user2.save()
+
+        feed_url = 'http://derek.stegelman.com/blog/feed/'
+        feed1 = Feed(user=user1, url=feed_url)
+        feed2 = Feed(user=user2, url=feed_url)
+        try:
+            feed1.save()
+            feed2.save()
+        except IntegrityError:
+            self.fail("Couldn't add %s for both %s and %s" % (feed_url, user1, user2))
+
+        self.assertIsNotNone(feed1.pk)
+        self.assertIsNotNone(feed2.pk)
+
