1329fdb10328f9afaaf2b76ca9838b2ea94a333d
Derek Stegelman <dstegelman@gmail.com>
2014-10-12 10:04:37 -0600

    South and 1.7 migrations.

--- feedme/migrations/0001_initial.py
+++ feedme/migrations/0001_initial.py
@@ -1,68 +1,64 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Category'
-        db.create_table(u'feedme_category', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=250, blank=True)),
-        ))
-        db.send_create_signal(u'feedme', ['Category'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-        # Adding model 'Feed'
-        db.create_table(u'feedme_feed', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
-            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['feedme.Category'], null=True, blank=True)),
-        ))
-        db.send_create_signal(u'feedme', ['Feed'])
-
-        # Adding model 'FeedItem'
-        db.create_table(u'feedme_feeditem', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=350, blank=True)),
-            ('link', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
-            ('content', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal(u'feedme', ['FeedItem'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Category'
-        db.delete_table(u'feedme_category')
-
-        # Deleting model 'Feed'
-        db.delete_table(u'feedme_feed')
-
-        # Deleting model 'FeedItem'
-        db.delete_table(u'feedme_feeditem')
-
-
-    models = {
-        u'feedme.category': {
-            'Meta': {'object_name': 'Category'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
-        },
-        u'feedme.feed': {
-            'Meta': {'object_name': 'Feed'},
-            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['feedme.Category']", 'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
-        },
-        u'feedme.feeditem': {
-            'Meta': {'object_name': 'FeedItem'},
-            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'link': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '350', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['feedme']
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=250, blank=True)),
+                ('slug', models.SlugField(null=True, editable=False, blank=True)),
+                ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Feed',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('link', models.CharField(max_length=450, blank=True)),
+                ('url', models.CharField(max_length=450, blank=True)),
+                ('title', models.CharField(max_length=250, null=True, blank=True)),
+                ('last_update', models.DateField(null=True, editable=False, blank=True)),
+                ('category', models.ForeignKey(blank=True, to='feedme.Category', null=True)),
+                ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='FeedItem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_fetched', models.DateField(auto_now_add=True, auto_created=True)),
+                ('title', models.CharField(max_length=350, blank=True)),
+                ('link', models.URLField(blank=True)),
+                ('content', models.TextField(blank=True)),
+                ('read', models.BooleanField(default=False)),
+                ('guid', models.CharField(max_length=255)),
+                ('pub_date', models.DateTimeField()),
+                ('feed', models.ForeignKey(blank=True, to='feedme.Feed', null=True)),
+            ],
+            options={
+                'ordering': ['id'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='feed',
+            unique_together=set([('url', 'user')]),
+        ),
+    ]
