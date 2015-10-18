ccffa7ec2b641f3d2703c6d8e4ccf5e92de795ff
Derek Stegelman <dstegelman@gmail.com>
2015-10-18 17:23:51 -0600

    Update tests.

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-__author__ = 'derek'
 import os
 import sys
 from django.conf import settings
@@ -8,46 +7,27 @@ import django
 
 DIRNAME = os.path.dirname(__file__)
 
-if django.VERSION[1] < 4:
-    # If the version is NOT django 4 or greater
-    # then remove the TZ setting.
-
-    settings.configure(DEBUG=True,
-                       DATABASES={
-                           'default': {
-                               'ENGINE': 'django.db.backends.sqlite3',
-                               }
-                       },
-                       ROOT_URLCONF='feedme.urls',
-                       INSTALLED_APPS=('django.contrib.auth',
-                                       'django.contrib.contenttypes',
-                                       'django.contrib.sessions',
-                                       'django.contrib.admin',
-                                       'feedme',))
-else:
-    settings.configure(DEBUG=True,
-                       DATABASES={
-                           'default': {
-                               'ENGINE': 'django.db.backends.sqlite3',
-                               }
-                       },
-                       ROOT_URLCONF='feedme.urls',
-                       INSTALLED_APPS=('django.contrib.auth',
-                                       'django.contrib.contenttypes',
-                                       'django.contrib.sessions',
-                                       'django.contrib.admin',
-                                       'feedme',),
-                       USE_TZ=True)
+settings.configure(DEBUG=True,
+                   DATABASES={
+                       'default': {
+                           'ENGINE': 'django.db.backends.sqlite3',
+                           }
+                   },
+                   ROOT_URLCONF='feedme.urls',
+                   INSTALLED_APPS=('django.contrib.auth',
+                                   'django.contrib.contenttypes',
+                                   'django.contrib.sessions',
+                                   'django.contrib.admin',
+                                   'feedme',),
+                   USE_TZ=True,
+                   MIDDLEWARE_CLASSES=('django.middleware.common.CommonMiddleware',
+                                       'django.middleware.csrf.CsrfViewMiddleware'))
 
 
-try:
-    # Django 1.7 needs this, but other versions dont.
-    django.setup()
-    from django.test.runner import DiscoverRunner
-    test_runner = DiscoverRunner(verbosity=1)
-except AttributeError:
-    from django.test.simple import DjangoTestSuiteRunner
-    test_runner = DjangoTestSuiteRunner(verbosity=1)
+
+django.setup()
+from django.test.runner import DiscoverRunner
+test_runner = DiscoverRunner(verbosity=1)
 failures = test_runner.run_tests(['feedme', ])
 
 from django.core.management import call_command
